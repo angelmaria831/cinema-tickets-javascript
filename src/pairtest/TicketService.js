a93dd21d5e6be3eb28fddaa23a1d456d616d1e0c
Angel Maria Das <angelmariadas@gmail.com>
2024-10-18 17:27:49 +0100

    chore: initialize basic variables and code standardization

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,12 +1,22 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
 
+  #maxTickets = 25;
+  #ticketPrices = {
+    ADULT: 25,
+    CHILD: 15,
+    INFANT: 0,
+  };
+
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
+    if (!Number.isInteger(accountId) || accountId <= 0) {
+      throw new InvalidPurchaseException("Invalid account ID");
+    }
   }
 }

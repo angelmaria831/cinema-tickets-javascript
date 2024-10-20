b0f3dce8b680a9d4ac0ab084cad22ba66c455c02
Angel Maria Das <angelmariadas@gmail.com>
2024-10-20 16:08:16 +0100

    feat: computing purchaseTickets - Added new method processTicketRequests - Calculate totalAmount, totalTickets and totalSeats from requests

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,5 +1,7 @@
 import TicketTypeRequest from "./lib/TicketTypeRequest.js";
 import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
+import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js";
+import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService.js"
 
 export default class TicketService {
   /**
@@ -18,5 +20,46 @@ export default class TicketService {
     if (!Number.isInteger(accountId) || accountId <= 0) {
       throw new InvalidPurchaseException("Invalid account ID");
     }
+
+    const {totalTickets, totalAmount, totalSeats } = this.#processTicketRequests(ticketTypeRequests);
+  
+    // For reciept generation - future
+    return {
+      totalTickets,
+      totalAmount,
+      totalSeats,
+    }
   }
+
+  #processTicketRequests(ticketTypeRequests) {
+    let totalTickets = 0;
+    let totalAmount = 0;
+    let totalSeats = 0;
+    let adultTickets = 0;
+    let childTickets = 0;
+    let infantTickets = 0;
+
+    for (const request of ticketTypeRequests) {
+      const ticketType = request.getTicketType();
+      const noOfTickets = request.getNoOfTickets();
+
+      totalTickets += noOfTickets;
+      totalAmount += noOfTickets * this.#ticketPrices[ticketType];
+      
+      if (ticketType === "INFANT") {
+        infantTickets += noOfTickets; // Infants don't need seats
+      }else if ( ticketType === "ADULT" || ticketType === "CHILD") {
+        totalSeats += noOfTickets;
+        (ticketType === "ADULT") ? adultTickets += noOfTickets : childTickets += noOfTickets;
+      }
+    }
+
+    return {
+      totalTickets,
+      totalAmount,
+      totalSeats,
+    }
+  }
+
+
 }

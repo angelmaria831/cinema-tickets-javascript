fb385411cb57662a6d7255900b8f3ebe2820e5ce
Angel Maria Das <angelmariadas@gmail.com>
2024-10-20 16:38:48 +0100

    test: cases for valid ticket requests - Calculate totalAmount considering tickettype - Do not reserve seats for Infants

--- src/pairtest/lib/InvalidPurchaseException.js
+++ src/pairtest/lib/InvalidPurchaseException.js
@@ -1,7 +1,7 @@
 export default class InvalidPurchaseException extends Error {
-    constructor(message = "Invalid Purchase", errorCode = null) {
-        super(message);
-        this.name = this.constructor.name;
-        this.errorCode = errorCode; // Optional error code for future extension
-    }
+  constructor(message = "Invalid Purchase", errorCode = null) {
+    super(message);
+    this.name = this.constructor.name;
+    this.errorCode = errorCode; // Optional error code for future extension
+  }
 }
--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -1,22 +1,80 @@
 import TicketService from "../src/pairtest/TicketService";
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
+import InvalidPurchaseException from "../src/pairtest/lib/InvalidPurchaseException";
+import TicketPaymentService from "../src/thirdparty/paymentgateway/TicketPaymentService";
+import SeatReservationService from "../src/thirdparty/seatbooking/SeatReservationService";
+
+jest.mock("../src/thirdparty/paymentgateway/TicketPaymentService.js");
+jest.mock("../src/thirdparty/seatbooking/SeatReservationService.js");
 
 describe("TicketService", () => {
-  const ticketService = new TicketService();
+  let ticketService;
+  const accountId = 1;
+
+  beforeEach(() => {
+    ticketService = new TicketService();
+  });
 
   it("should throw error for invalid accountId", () => {
     const ticketRequest = new TicketTypeRequest("ADULT", 2);
 
     expect(() => {
       ticketService.purchaseTickets(0, ticketRequest);
-    }).toThrow();
+    }).toThrow(InvalidPurchaseException);
 
     expect(() => {
       ticketService.purchaseTickets(3.5, ticketRequest);
-    }).toThrow();
+    }).toThrow(InvalidPurchaseException);
 
     expect(() => {
       ticketService.purchaseTickets(-6, ticketRequest);
-    }).toThrow();
+    }).toThrow(InvalidPurchaseException);
+  });
+
+  it("should successfully compute the totalAmount and totalSeats for valid requests", () => {
+    const requests = new TicketTypeRequest("ADULT", 3);
+    ticketService.purchaseTickets(accountId, requests);
+
+    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
+      1,
+      75, // Tickets for 3 Adults are charged
+    );
+    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
+      1,
+      3, // Seats for 3 Adults are reserved
+    );
+
+    const requestsWithChild = [
+      new TicketTypeRequest("ADULT", 2),
+      new TicketTypeRequest("CHILD", 1),
+    ];
+
+    ticketService.purchaseTickets(accountId, ...requestsWithChild);
+    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
+      1,
+      65, // Adult + Child tickets are charged
+    );
+    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
+      1,
+      3, // Adult + Child seats are reserved
+    );
+  });
+
+  it("should not reserve seats for infants", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 2),
+      new TicketTypeRequest("CHILD", 1),
+      new TicketTypeRequest("INFANT", 1),
+    ];
+    ticketService.purchaseTickets(accountId, ...requests);
+
+    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
+      1,
+      65, // Adult + Child tickets are charged
+    );
+    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
+      1,
+      3, // Seat is not reserved for infant
+    );
   });
 });

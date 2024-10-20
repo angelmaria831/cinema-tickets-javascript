7f032d6fc1cf90432e08dac75919490b96eaa5b4
Angel Maria Das <angelmariadas@gmail.com>
2024-10-20 15:47:31 +0100

    feat: customize InvalidPurchaseException class - Default message set to 'Invalid Purchase'. - Added optional errorCode property for enhanced error handling.

--- src/pairtest/lib/InvalidPurchaseException.js
+++ src/pairtest/lib/InvalidPurchaseException.js
@@ -1,1 +1,7 @@
-export default class InvalidPurchaseException extends Error {}
+export default class InvalidPurchaseException extends Error {
+    constructor(message = "Invalid Purchase", errorCode = null) {
+        super(message);
+        this.name = this.constructor.name;
+        this.errorCode = errorCode; // Optional error code for future extension
+    }
+}

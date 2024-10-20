27b2cafc863b9c2c1729238e0ba550a47036e3b9
Angel Maria Das <angelmariadas@gmail.com>
2024-10-20 16:58:02 +0100

    test: validate ticket purchase - Can't purchase if ticket count is greater than 25 - Can't purchase ticket without atleast one adult

--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -31,6 +31,78 @@ describe("TicketService", () => {
     }).toThrow(InvalidPurchaseException);
   });
 
+  it("should not reserve seats for infants", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 2),
+      new TicketTypeRequest("CHILD", 1),
+      new TicketTypeRequest("INFANT", 1),
+    ];
+    ticketService.purchaseTickets(accountId, ...requests);
+
+    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
+      1,
+      65, // Adult + Child tickets are charged
+    );
+    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
+      1,
+      3, // Seat is not reserved for infant
+    );
+  });
+
+  it("should throw error for more than 25 tickets purchase", () => {
+    const requests = new TicketTypeRequest("ADULT", 26);
+    expect(() => {
+      ticketService.purchaseTickets(accountId, requests);
+    }).toThrow(InvalidPurchaseException);
+
+    const requestsWithChild = [
+      new TicketTypeRequest("ADULT", 20),
+      new TicketTypeRequest("CHILD", 6),
+    ];
+    expect(() => {
+      ticketService.purchaseTickets(accountId, ...requestsWithChild);
+    }).toThrow(InvalidPurchaseException);
+
+    const requestsWithInfant = [
+      new TicketTypeRequest("ADULT", 20),
+      new TicketTypeRequest("CHILD", 5),
+      new TicketTypeRequest("INFANT", 1),
+    ];
+    expect(() => {
+      ticketService.purchaseTickets(accountId, ...requestsWithInfant);
+    }).toThrow(InvalidPurchaseException);
+  });
+
+  it("should be able buy tickets upto 25", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 20),
+      new TicketTypeRequest("CHILD", 4),
+      new TicketTypeRequest("INFANT", 1),
+    ];
+    ticketService.purchaseTickets(accountId, ...requests);
+
+    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
+      1,
+      560, // Adult + Child tickets are charged
+    );
+    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
+      1,
+      24, // Seat reserved for 20 Adults and 4 Children
+    );
+  });
+
+  it("should throw error if child or infant tickets are purchased without an adult", () => {
+    const requestWithOnlyChild = new TicketTypeRequest("CHILD", 1);
+    expect(() => {
+      ticketService.purchaseTickets(accountId, requestWithOnlyChild);
+    }).toThrow(InvalidPurchaseException);
+
+    const requestWithOnlyInfant = new TicketTypeRequest("INFANT", 1);
+    expect(() => {
+      ticketService.purchaseTickets(accountId, requestWithOnlyInfant);
+    }).toThrow(InvalidPurchaseException);
+  });
+
   it("should successfully compute the totalAmount and totalSeats for valid requests", () => {
     const requests = new TicketTypeRequest("ADULT", 3);
     ticketService.purchaseTickets(accountId, requests);
@@ -60,21 +132,4 @@ describe("TicketService", () => {
     );
   });
 
-  it("should not reserve seats for infants", () => {
-    const requests = [
-      new TicketTypeRequest("ADULT", 2),
-      new TicketTypeRequest("CHILD", 1),
-      new TicketTypeRequest("INFANT", 1),
-    ];
-    ticketService.purchaseTickets(accountId, ...requests);
-
-    expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(
-      1,
-      65, // Adult + Child tickets are charged
-    );
-    expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(
-      1,
-      3, // Seat is not reserved for infant
-    );
-  });
 });
